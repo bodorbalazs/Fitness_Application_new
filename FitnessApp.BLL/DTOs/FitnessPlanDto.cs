5a1f4212f353a9517652670cea4b0cac5cd68922
bodorbalazs <bodorbalazs444@gmail.com>
2023-03-23 16:08:54 +0100

    Dtos changed

--- FitnessApp.BLL/DTOs/FitnessPlanDto.cs
+++ FitnessApp.BLL/DTOs/FitnessPlanDto.cs
@@ -7,7 +7,7 @@ namespace Fitness_Application_new.DTOs
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public ICollection<FitnessExercise> Exercises { get; set; }
+        public ICollection<FitnessExerciseDto> Exercises { get; set; }
         public string ApplicationUserId { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
     }
