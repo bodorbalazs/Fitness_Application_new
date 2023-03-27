7f2d50582d7af2526e7f8d762df528910166e530
bodorbalazs <bodorbalazs444@gmail.com>
2023-03-27 12:22:17 +0200

    AutoMapper works now

---
+++ Fitness_Application_new/Automapper/MapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Fitness_Application_new.DTOs;
+using FitnessApp.DAL.Models;
+
+namespace FitnessApp.API.Automapper
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<FavouriteItem, FavouriteItemDto>().ReverseMap();
+            CreateMap<FitnessPlan, FitnessPlanDto>().ReverseMap();
+            CreateMap<FitnessExercise, FitnessExerciseDto>().ReverseMap();
+            CreateMap<Rating, RatingDto>().ReverseMap();
+        }
+    }
+}
