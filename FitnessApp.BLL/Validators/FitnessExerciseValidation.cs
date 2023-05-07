f565f2bc95a8f3295161c276a0affc8b03812fe7
bodorbalazs <bodorbalazs444@gmail.com>
2023-05-07 13:34:16 +0200

    avg rating finished, validation in progress

---
+++ FitnessApp.BLL/Validators/FitnessExerciseValidation.cs
@@ -0,0 +1,18 @@
+﻿using Fitness_Application_new.DTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.BLL.Validators
+{
+    public class FitnessExerciseValidation : AbstractValidator<FitnessExerciseDto>
+    {
+        public FitnessExerciseValidation()
+        {
+
+        }
+    }
+}
