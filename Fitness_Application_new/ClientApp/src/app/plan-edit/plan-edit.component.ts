e012d7a2a0b2b18c9db8295ef533422fe2f631f4
bodorbalazs <bodorbalazs444@gmail.com>
2023-05-30 23:23:12 +0200

    edit exercises now works, picture edit in progress

--- Fitness_Application_new/ClientApp/src/app/plan-edit/plan-edit.component.ts
+++ Fitness_Application_new/ClientApp/src/app/plan-edit/plan-edit.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { FitnessExerciseClient, FitnessExerciseDto, FitnessPlanClient, FitnessPlanDto } from '../clientservice/api.client';
+import { NgbModal,ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-plan-edit',
@@ -14,14 +16,43 @@ export class PlanEditComponent {
   id!:number;
   specifiedPlan! : FitnessPlanDto;
   updateForm!: FormGroup ;
+  editFitnessExerciseForm: FormGroup;
+  fitnessExercises: FitnessExerciseDto[]=[];
+  specifiedFitnessExercises: FitnessExerciseDto[]=[];
+  closeResult:string ="";
+  filedata:any = undefined;
   constructor(private route: ActivatedRoute,
     private FitnessPlanService : FitnessPlanClient,
     private router: Router,
-    private formBuilder: FormBuilder)
-    { }
+    private formBuilder: FormBuilder,
+    private FitnessExerciseService : FitnessExerciseClient,
+    private modalService:NgbModal,
+    private http:HttpClient)
+    {
+      this.editFitnessExerciseForm = this.formBuilder.group({
+        id:0,
+        name:'',
+        description:'',
+        pictureUrl:'',
+        difficulty:'',
+      })
+
+
+     }
     ngOnInit(): void {
       this.id = parseInt(this.route.snapshot.paramMap.get('id') || '{}');
-      this.FitnessPlanService.get(this.id).subscribe(element => this.specifiedPlan = element);
+      this.FitnessPlanService.get(this.id).subscribe(element => this.specifiedPlan = element );
+      
+
+      this.FitnessExerciseService.getAll().subscribe(element =>
+        {
+          this.fitnessExercises= element;
+          this.fitnessExercises.forEach(exercise => {
+            if(exercise.fitnessPlanId== this.id){
+              this.specifiedFitnessExercises.push(exercise);
+            }
+          });
+        });
 
       this.updateForm = this.formBuilder.group({
         name: this.formBuilder.control(this.specifiedPlan?.name),
@@ -43,4 +74,68 @@ export class PlanEditComponent {
    onSelectBack(){
     this.router.navigate(['/edit-fitness-plans'])
    }
+
+   fileEvent(e:any){
+    this.filedata = e.target.files[0];
+    console.log("FILE SAVED");
+  }
+   open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  private getDismissReason(reason: any): string {
+    this.filedata=undefined;
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  EditExerciseList(exerciseId:number){
+
+    this.FitnessExerciseService.put(exerciseId,new FitnessExerciseDto({
+                id:exerciseId,
+                name:this.editFitnessExerciseForm.get('name')?.value,
+                description:this.editFitnessExerciseForm.get('description')?.value,
+                pictureUrl:this.editFitnessExerciseForm.get('pictureUrl')?.value,
+                difficulty:this.editFitnessExerciseForm.get('difficulty')?.value,
+                fitnessPlanId:this.id
+                })
+                ).subscribe();
+
+                if(this.filedata!=undefined){
+      
+                  var myFormData = new FormData();
+                  const headers = new HttpHeaders();
+                  headers.append('Content-Type', 'multipart/form-data');
+                  headers.append('Accept', 'application/json');
+                  myFormData.append('image', this.filedata.picture);
+                  myFormData.append('id',exerciseId.toString());
+                  /* Image Post Request */
+                  this.http.post('https://localhost:7252/api/FitnessExercise/SavePicture', myFormData, {
+                  headers: headers
+                  }).subscribe(data => {
+                   //Check success message
+                   console.log(data);
+                  });
+                }
+      this.filedata = undefined;
+  }
+
+  selectexercise(exercise:FitnessExerciseDto){
+    this.editFitnessExerciseForm = this.formBuilder.group({
+      id: this.formBuilder.control(exercise.id),
+      name:this.formBuilder.control(exercise.name),
+      description:this.formBuilder.control(exercise.description),
+      pictureUrl:'',
+      difficulty:this.formBuilder.control(exercise.difficulty),
+    })
+  }
+
 }
--- Fitness_Application_new/Controllers/FitnessExerciseController.cs
+++ Fitness_Application_new/Controllers/FitnessExerciseController.cs
@@ -50,7 +50,14 @@ namespace Fitness_Application_new.Controllers
             try
             {
                 //Path.Combine(_environment.WebRootPath, "FolderNameOfYourWWWRoot", Image.FileName);
-                string path = Path.Combine(@"C:\Users\bodor\OneDrive\Desktop\egyetemshit\MSC\Dipterv1\Fitness_Application_new\ClientApp\src\assets\images", id);//image.FileName);
+                string path = Path.Combine(@"C:\Users\bodor\OneDrive\Desktop\egyetemshit\MSC\Dipterv1\Fitness_Application_new\ClientApp\src\assets\images", id);
+
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                }
+
+                
                 using (Stream stream = new FileStream(path, FileMode.Create))
                 {
                     await image.CopyToAsync(stream);
