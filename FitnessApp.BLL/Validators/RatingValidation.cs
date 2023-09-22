176793319432103198b4641ea9d832040bd8e714
bodorbalazs <bodorbalazs444@gmail.com>
2023-09-22 11:05:28 +0200

    first integration test works

--- FitnessApp.BLL/Validators/RatingValidation.cs
+++ FitnessApp.BLL/Validators/RatingValidation.cs
@@ -13,7 +13,6 @@ namespace FitnessApp.BLL.Validators
     {
         public RatingValidation()
         {
-            RuleFor(x=> x.Id).NotEmpty().NotNull().WithMessage("no Id given for rating");
             RuleFor(x=> x.value).NotEmpty().InclusiveBetween(0,10).WithMessage("Rating is not between 0 and 10");
         }
     }
---
+++ FitnessApp.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,36 @@
+﻿using Fitness_Application_new.Interfaces;
+using FitnessApp.DAL.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.IntegrationTests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        public Mock<IRatingService> RatingServiceMock { get; }
+        public CustomWebApplicationFactory()
+        {
+            RatingServiceMock= new Mock<IRatingService>();
+        }
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddSingleton(RatingServiceMock.Object);
+            });
+        }
+    }
+}
