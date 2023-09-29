f39f1c4b84572eff1c13c2321d34a33846ae73c0
bodorbalazs <bodorbalazs444@gmail.com>
2023-09-29 17:14:28 +0200

    small db fix

--- FitnessApp.DAL/Data/ApplicationDbContext.cs
+++ FitnessApp.DAL/Data/ApplicationDbContext.cs
@@ -14,10 +14,6 @@ namespace FitnessApp.DAL.Data
         {
 
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DiptervFitness;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-        }
 
         /*protected override void OnModelCreating(ModelBuilder builder)
         {
