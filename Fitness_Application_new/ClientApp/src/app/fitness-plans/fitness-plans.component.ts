106995ec21ae3c78667fc0a2f2d1a87007cdc9c2
bodorbalazs <bodorbalazs444@gmail.com>
2023-10-13 15:53:49 +0200

    fitness plans menu no longer throws 401 error when not logged in

--- Fitness_Application_new/ClientApp/src/app/fitness-plans/fitness-plans.component.ts
+++ Fitness_Application_new/ClientApp/src/app/fitness-plans/fitness-plans.component.ts
@@ -34,39 +34,10 @@ export class FitnessPlansComponent implements OnInit {
         });
       });
     });
-    this.favouriteService.getUsersPlans().subscribe(element => this.favouriteList=element);
   }
   
   onSelectPlan(id:number){
     this.router.navigate(['/plan-details',id])
-  }
-  FavouritePressed(id: number){
-    var exists=false;
-    this.favouriteList.forEach(element => {
-      if(element.fitnessPlanId == id){
-        this.favouriteService.delete(element.id).subscribe();
-        exists=true;
-      }
-    });
-    if(!exists){
-      this.favouriteService.addFavouriteItem(
-        new FavouriteItemDto({
-            id:0,
-            fitnessPlanId:id
-        })).subscribe();
-    }
-    
-    this.favouriteService.getUsersPlans().subscribe(element => this.favouriteList=element);
-  }
-  favouriteExists(id:number):boolean{
-    var returnval=false;
-    this.favouriteList.forEach(element => {
-      if(element.fitnessPlanId == id){
-        this.favouriteService.delete(element.id).subscribe();
-        returnval=true;
-      }
-    });
-    return returnval;
   }
 
 }
